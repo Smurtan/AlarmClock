082dbe4bcc6c4b119192865c6133ad33d3189844
Yycheslav <smirn.005vychislavoo@gmail.com>
2022-10-04 07:44:51 +0300

    Continue to work on the design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtCore import QSize, QTimer, QRect
+from PyQt6.QtCore import QSize, QTimer, QRect, Qt
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import (
 QMainWindow, QApplication, QFrame, QLabel
@@ -36,13 +36,19 @@ class MainWindow(QMainWindow):
         self.font_time = QFont()
         self.font_time.setFamily("Segoe UI")
         self.font_time.setPointSize(75)
-        self.font_time.setWeight(50)
 
         self.time_label = QLabel(self.header)
-        self.time_label.setGeometry(QRect(220, 60, 291, 131))
+        #self.time_label.setGeometry(QRect(195, 47, 290, 130))
         self.time_label.setFont(self.font_time)
         self.time_label.setText('00:00')
         self.time_label.setProperty("class", "time_label")
+        self.time_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.alarm_clock_area = QFrame(self.circle_bg)
+        self.alarm_clock_area.setGeometry(QRect(0, 270, 620, 350))
+        self.alarm_clock_area.setProperty("class", "alarm_clock_area")
+
+
 
         self.setCentralWidget(self.container)
 
