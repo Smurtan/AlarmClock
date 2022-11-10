6df54d647f3e1e3b6f80e1aa64f5863751e50763
Yycheslav <smirn.005vychislavoo@gmail.com>
2022-11-10 17:58:32 +0300

    Solving a problem with the toggle switch animation

--- py_toggle/py_toggle.py
+++ py_toggle/py_toggle.py
@@ -1,6 +1,8 @@
-from PyQt6.QtCore import Qt, QRect
+from PyQt6.QtCore import (Qt, QRect, QPoint, QEasingCurve,
+                          QPropertyAnimation)
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QCheckBox
+from astroid.objects import Property
 
 
 class PyToggle(QCheckBox):
@@ -9,7 +11,8 @@ class PyToggle(QCheckBox):
             width=60,
             bg_color="#777",
             circle_color="#DDD",
-            active_color="#00BCff"
+            active_color="#00BCff",
+            animation_curve=QEasingCurve.Type.OutBounce
     ):
         QCheckBox.__init__(self)
 
@@ -22,6 +25,42 @@ class PyToggle(QCheckBox):
         self._circle_color = circle_color
         self._activate_color = active_color
 
+        # CREAT ANIMATION
+        self._circle_position = 3
+        self.animation = QPropertyAnimation(self, b"circle_position", self)
+        self.animation.setEasingCurve(animation_curve)
+        self.animation.setDuration(500)  # Time in milliseconds
+
+        # CONNECT STATE CHANGED
+        self.stateChanged.connect(self.start_transition)
+
+    # CREAT NEW SET AND GET PROPERTY
+    @Property(float)  # Get
+    def circle_position(self):
+        return self._circle_position
+
+    @circle_position.setter
+    def circle_position(self, pos):
+        self._circle_position = pos
+        self.update()
+
+    def start_transition(self, value):
+        self.animation.stop()  # Stop animation if running
+        if value:
+            self.animation.setEndValue(self.width() - 26)
+        else:
+            self.animation.setEndValue(3)
+
+        # START ANIMATION
+        self.animation.start()
+
+        print(f'Status: {self.isChecked()}')
+
+    # SET NEW HIT AREA
+    def hitButton(self, pos: QPoint):
+        return self.contentsRect().contains(pos)
+
+    # DRAW NEW ITEMS
     def paintEvent(self, e):
         # SET PAINTER
         p = QPainter(self)
@@ -33,9 +72,23 @@ class PyToggle(QCheckBox):
         # DRAW RECTANGLE
         rect = QRect(0, 0, self.width(), self.height())
 
-        # DRAW BG
-        p.setBrush(QColor(self._bg_color))
-        p.drawRoundedRect(0, 0, rect.width(), self.height(), self.height() / 2, self.height() / 2)
+        # CHECK IF IS CHECKED
+        if not self.isChecked():
+            # DRAW BG
+            p.setBrush(QColor(self._bg_color))
+            p.drawRoundedRect(0, 0, rect.width(), self.height(), self.height() / 2, self.height() / 2)
+
+            # DRAW CIRCLE
+            p.setBrush(QColor(self._circle_color))
+            p.drawEllipse(self._circle_position, 3, 22, 22)
+        else:
+            # DRAW BG
+            p.setBrush(QColor(self._activate_color))
+            p.drawRoundedRect(0, 0, rect.width(), self.height(), self.height() / 2, self.height() / 2)
+
+            # DRAW CIRCLE
+            p.setBrush(QColor(self._circle_color))
+            p.drawEllipse(self._circle_position, 3, 22, 22)
 
         # END DRAW
         p.end()
